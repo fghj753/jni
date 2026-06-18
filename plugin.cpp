0028545164796fc60693e7a730b31fc7dc21e405
fghj753 <leonenen2000@gmail.com>
2026-06-18 04:57:50 +0300

    Update plugin.cpp

--- plugin.cpp
+++ plugin.cpp
@@ -1,45 +1,10 @@
 #include <jni.h>
 #include <android/log.h>
-#include <dlfcn.h>
-#include <cstring>
-#include <unistd.h>
-#include <thread>
-#include <link.h>
-#include "vendor/Dobby/include/dobby.h"
 
-typedef bool (*tConnect)(void*, const char*, uint16_t, uint16_t, uint32_t, int);
-tConnect orig_Connect = nullptr;
-
-bool hook_Connect(void* _this, const char* host, uint16_t port, uint16_t cPort, uint32_t dep, int sleep) {
-    return orig_Connect(_this, "188.127.241.74", 5014, cPort, dep, sleep);
-}
-
-void InitializeHooks() {
-    uintptr_t base = 0;
-    
-    while (!base) {
-        dl_iterate_phdr([](dl_phdr_info* info, size_t, void* data) {
-            if (info->dlpi_name && strstr(info->dlpi_name, "libblackrussia-client.so")) {
-                *(uintptr_t*)data = info->dlpi_addr;
-                return 1;
-            }
-            return 0;
-        }, &base);
-        if (!base) usleep(100000);
-    }
-
-    uintptr_t rakClient = 0;
-    while (!(rakClient = *(uintptr_t*)(base + 0x12DB30))) {
-        usleep(100000);
-    }
-
-    uintptr_t vtable = *(uintptr_t*)rakClient;
-    void* target = (void*)*(uintptr_t*)(vtable + 32); // <- ИСПРАВЛЕНО!
-
-    DobbyHook(target, (void*)hook_Connect, (void**)&orig_Connect);
-}
+#define LOG_TAG "Plugin"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
-    std::thread(InitializeHooks).detach();
+    LOGD("================== PLUGIN LOADED SUCCESSFULLY ==================");
     return JNI_VERSION_1_6;
 }
